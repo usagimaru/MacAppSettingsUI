85719f1ce59e82b12c9b9f8c79bf5c52c4cba3c7
usagimaru <usagimaru@clickandmagic.com>
2026-06-20 23:46:32 +0900

    add alternative setContentContainerView()

--- Sources/SettingsPaneLayoutGuide.swift
+++ Sources/SettingsPaneLayoutGuide.swift
@@ -17,27 +17,35 @@ public extension SettingsPaneLayoutGuide {
 	
 	/// Prepare container view if you want
 	func setContentContainerView(maximumWidth: CGFloat?, labelLayoutGuideWidth: CGFloat? = nil) {
+		let targetView: NSView?
+		if let vc = self as? NSViewController {
+			targetView = vc.view
+		}
+		else if let view = self as? NSView {
+			targetView = view
+		}
+		else {
+			targetView = nil
+		}
+
+		guard let targetView else { return }
+		setContentContainerView(to: targetView, maximumWidth: maximumWidth, labelLayoutGuideWidth: labelLayoutGuideWidth)
+	}
+
+	/// Prepare container view to a specific subview
+	func setContentContainerView(to targetView: NSView, maximumWidth: CGFloat?, labelLayoutGuideWidth: CGFloat? = nil) {
 		if let contentContainerView {
 			contentContainerView.removeFromSuperview()
 		}
-		
+
 		let containerView = SettingsPaneContainerView()
 		containerView.containerMaximumWidth = maximumWidth
-		
+
 		if let labelLayoutGuideWidth {
 			containerView.labelLayoutGuideWidth = labelLayoutGuideWidth
 		}
-		
-		if let vc = self as? NSViewController {
-			vc.view.addSubview(containerView, positioned: .below, relativeTo: nil)
-		}
-		else if let view = self as? NSView {
-			view.addSubview(containerView, positioned: .below, relativeTo: nil)
-		}
-		else {
-			return
-		}
-		
+
+		targetView.addSubview(containerView, positioned: .below, relativeTo: nil)
 		contentContainerView = containerView
 	}
 	
